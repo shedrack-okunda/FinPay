4644e8985ae82146a4b2a79de7bf3fbb5ce67bef
Sheddy <okundashedrack@gmail.com>
2025-09-11 10:03:07 +0300

    reorder routes

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -10,12 +10,6 @@ import { authenticateToken } from "../middleware/auth";
 
 const router = express.Router();
 
-// GET all users (later you can restrict to admin)
-router.get("/", authenticateToken, getAllUsers);
-
-// GET user by ID
-router.get("/:id", authenticateToken, getUserById);
-
 // GET current logged-in user profile
 router.get("/me/profile", authenticateToken, getCurrentUser);
 
@@ -24,5 +18,11 @@ router.put("/me/profile", authenticateToken, updateUserProfile);
 
 // DELETE current user
 router.delete("/me", authenticateToken, deleteUser);
+
+// GET user by ID
+router.get("/:id", authenticateToken, getUserById);
+
+// GET all users (later you can restrict to admin)
+router.get("/", authenticateToken, getAllUsers);
 
 export default router;
